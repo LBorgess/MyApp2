fb05aa10f229de121db6dd650ab06fe841efe54a
LBorgess <lucas_rancer@hotmail.com>
2023-03-30 21:47:00 -0300

    menu 2

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -3,12 +3,30 @@ package br.com.etecia.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
+    // Variavel global para representar o xml
+    MaterialToolbar idToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Apresentando o XML para o Java
+        idToolBar = findViewById(R.id.idToolBar);
+
+        idToolBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Hmmmm", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // idToolBar.setOnMenuItemClickListener(new );
     }
 }
