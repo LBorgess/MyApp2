e3ed24c3249936b514308e10f6cbc07eb2dc8455
LBorgess <lucas_rancer@hotmail.com>
2023-03-30 21:56:05 -0300

    menu 3

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,8 +1,10 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -23,10 +25,34 @@ public class MainActivity extends AppCompatActivity {
         idToolBar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Hmmmm", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Burger", Toast.LENGTH_SHORT).show();
             }
         });
 
-        // idToolBar.setOnMenuItemClickListener(new );
+        idToolBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                switch (item.getItemId()) {
+                    case R.id.mFavorite:
+                        Toast.makeText(getApplicationContext(), "FAVORITEI!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mBuscar:
+                        Toast.makeText(getApplicationContext(), "ACHOUU!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mCompartilhar:
+                        Toast.makeText(getApplicationContext(), "BLOGUEIRINHA!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mSalvar:
+                        Toast.makeText(getApplicationContext(), "TÁ SAVE!", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.mSair:
+                        Toast.makeText(getApplicationContext(), "VALEU FALOW!", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return false;
+            }
+        });
     }
 }
